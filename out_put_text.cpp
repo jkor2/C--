17b47c72a058d6ce0fc7b50556f137f25d5b02f1
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 19:01:59 -0400

    Multiple at once

--- out_put_text.cpp
+++ out_put_text.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main()
 {
+    // Flush will output on the same line as next output
     cout << "Starting the program..." << flush;
 
     cout << "Hello" << endl;
+
+    cout << "This"
+         << " Is"
+         << " Mutliple outputs"
+         << " at once" << endl;
 
     cout << "More text output..." << endl;
 }
