f742e8ea2130565c83c5402e6b2a0975021bebf0
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 19:29:35 -0400

    Formatting changes

--- variables.cpp
+++ variables.cpp
@@ -6,5 +6,5 @@ int main()
     // Storing an integer
     int numberOfSomething = 4;
 
-    cout << "Number of Dogs" << numberOfSomething << endl;
+    cout << "Number of Dogs " << numberOfSomething << endl;
 }
