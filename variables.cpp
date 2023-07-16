fcda5ea8197d9e90c116a87efac2613af1ea3d72
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 19:40:09 -0400

    Reasing variables in C++

--- variables.cpp
+++ variables.cpp
@@ -8,4 +8,11 @@ int main()
     int totalNumberOfAnimals = 9;
     cout << "Number of Dogs " << numberOfSomething << endl;
     cout << "I have " << totalNumberOfAnimals - numberOfSomething << " animals that are not dogs" << endl;
+
+    // Reasing varibale names
+    cout << "You have another animal!" << endl;
+
+    numberOfSomething = numberOfSomething + 1;
+
+    cout << "You now have " << numberOfSomething << " dogs." << endl;
 }
