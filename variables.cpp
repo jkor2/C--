78c0d39aab9d8af921351dec8600ecf53d6a380d
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 19:37:13 -0400

    Artimetic in c++

--- variables.cpp
+++ variables.cpp
@@ -5,6 +5,7 @@ int main()
 {
     // Storing an integer
     int numberOfSomething = 4;
-
+    int totalNumberOfAnimals = 9;
     cout << "Number of Dogs " << numberOfSomething << endl;
+    cout << "I have " << totalNumberOfAnimals - numberOfSomething << " animals that are not dogs" << endl;
 }
