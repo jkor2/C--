6185028d6f5268f0b250268d64d4174d9960e276
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 20:45:17 -0400

    Playing around with User Inputs in C++

--- input.cpp
+++ input.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Programming is starting..." << endl;
+    cout << "Enter your name: " << flush;
+
+    // Only have to declare the variable
+    // dont need to set it to an empty string
+    string input;
+
+    cin >> input;
+
+    cout << "Hello " << input << endl;
+
+    cout << "how old are you " << input << "? " << flush;
+
+    int value;
+
+    cin >> value;
+
+    cout << "You are " << value << " years old." << endl;
 }
