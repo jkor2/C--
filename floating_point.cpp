ab1dbdaa5ffcac594531eaede0d75d2e9ecaba46
Jake Korobellis <jkorobellis@gmail.com>
2023-07-17 11:39:29 -0400

    Adding fixed/setPrecsion

--- floating_point.cpp
+++ floating_point.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,5 +8,7 @@ int main()
     // Float type (Decimals in integrers)
     float value = 77.1;
 
-    cout << "Yo" << value << endl;
+    // Fixed variable - Preventing scientific notation
+    // Set Precsion - sets the number of significant numbers to be output
+    cout << "Yo" << setprecision(20) << fixed << value << endl;
 }
