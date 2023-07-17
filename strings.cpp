e61fa364b12ff7016507c917bd7715e95d432daf
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 20:04:36 -0400

    Concatinating a string

--- strings.cpp
+++ strings.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Ready to go" << endl;
+    cout << "Starting porgram..." << endl;
+
+    string value = "Hello, this is a string!";
+    string value2 = "This is the second string";
+    cout << "First string: " << value << endl;
+    cout << "Second string: " << value2 << endl;
+
+    // concat string
+    cout << "Concat strings: " << value + " " + value2 << endl;
 }
