0bf83ed46be61212613990299ab0cb89f05f6986
Jake Korobellis <jkorobellis@gmail.com>
2023-07-16 22:56:43 -0400

    Short INT

--- integer_types.cpp
+++ integer_types.cpp
@@ -16,4 +16,8 @@ int main()
     // For an interger greater or less than MAX && MIN
     long long int value = 123456789987654321;
     cout << "Long INT Value: " << value << endl;
+
+    // Short INT
+    short int value_short = 12345;
+    cout << value_short << endl;
 }
