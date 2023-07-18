bd2e59d4d96a9fff9e8b3ff7eaa4182c34af5c07
Jake Korobellis <jkorobellis@gmail.com>
2023-07-18 18:19:35 -0400

    Going off the top of my head -- basic arithmetic

---
+++ basic_practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome, what kind of math would you like to do?" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Square Root" << endl;
+
+    cout << "Please choose > " << flush;
+    int choice;
+    cin >> choice;
+}
