7373d2decb2e5ed81f751319fb1a70bdbc793e26
Jake Korobellis <jkorobellis@gmail.com>
2023-07-18 22:05:46 -0400

    Storing condtions in booleans

---
+++ condtions.cpp
@@ -0,0 +1,17 @@
+// Complex condtions in C++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Starting Program" << endl;
+
+    int val1 = 6;
+    int val2 = 0;
+
+    // Storing condtions within booleans
+
+    bool check_conditon = val1 > 5 && val2 < 1;
+    cout << "Check condtion: " << check_conditon << endl;
+}
