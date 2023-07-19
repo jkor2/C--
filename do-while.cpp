47ac26e3d3f5aa2b3a15a51305d98dff963ea058
Jake Korobellis <jkorobellis@gmail.com>
2023-07-19 13:49:28 -0400

    do/while loop

---
+++ do-while.cpp
@@ -0,0 +1,18 @@
+// I already know this loop -- just for addtional practice
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value = 20;
+
+    do
+    {
+        value--;
+        cout << "Hello World x Spam";
+    } while (value != 0);
+    {
+        cout << "Complete" << endl;
+    }
+}
