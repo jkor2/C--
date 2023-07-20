359143357a4ca5ca1cb7b810fda4964f7753aa0d
Jake Korobellis <jkorobellis@gmail.com>
2023-07-19 22:47:25 -0400

    Better example of break/continue in do/while

--- break-continue.cpp
+++ break-continue.cpp
@@ -5,21 +5,38 @@ using namespace std;
 int main()
 {
     cout << "Starting Program" << endl;
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "i is " << i << endl;
-
-        if (i == 3)
+    /*
+        for (int i = 0; i < 5; i++)
         {
-            // Once I == 3, break and quit the loop
-            break;
+            cout << "i is " << i << endl;
+
+            if (i == 3)
+            {
+                // Once I == 3, break and quit the loop
+                break;
+            }
+            else
+            {
+                // Dont need technically need this -
+                // However, for practice adding
+                continue;
+            }
+        }
+        */
+    const string password = "HelloWorld";
+    string input;
+    do
+    {
+        cout << "Enter the password > " << flush;
+        cin >> input;
+
+        if (input == password)
+        {
+            break; // <- Input is correct break
         }
         else
         {
-            // Dont need technically need this -
-            // However, for practice adding
-            continue;
+            continue; // <- Input is incorrect continue
         }
-    }
+    } while (true); // <- Setting true, no way to set false = infinite loop
 }
