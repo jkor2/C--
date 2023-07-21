4c02326b513f3e918e00efdb36afafc1985ecad4
Jake Korobellis <jkorobellis@gmail.com>
2023-07-21 17:33:27 -0400

    Switch Case in c++

---
+++ switch.cpp
@@ -0,0 +1,31 @@
+// Basically same as if, else if, & else.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int value = 6;
+
+    switch (value)
+    {
+    case 1:
+        cout << "Value is 1" << endl;
+        break;
+    case 2:
+        cout << "value is 2" << endl;
+        break;
+    case 3:
+        cout << "Value is 3" << endl;
+        break;
+    case 4:
+        cout << "Value is 4" << endl;
+        break;
+    case 5:
+        cout << "Value is 5" << endl;
+        break;
+    default:
+        cout << "Value given is not compatibale" << endl;
+        break;
+    }
+}
