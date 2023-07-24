de7c90352be5e3a04e50cc35120d21f3200faa6c
Jake Korobellis <jkorobellis@gmail.com>
2023-07-23 23:09:13 -0400

    Defining a function at the top

--- prototypesandincludes.cpp
+++ prototypesandincludes.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
+// Defining a function
+void doSomething();
+
 int main()
 {
     cout << "Connected" << endl;
+}
+
+void doSomething()
+{
+    cout << "Hello" << endl;
 }
