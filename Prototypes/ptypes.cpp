417ebe856eccd3eba38ecb672e6810b2b531762f
Jake Korobellis <jkorobellis@gmail.com>
2023-07-23 23:19:30 -0400

    utils.h included within main

---
+++ Prototypes/ptypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "utlis.h"
+
+using namespace std;
+
+int main()
+{
+
+    doSomething();
+    doSomething();
+}
+
+void doSomething()
+{
+    cout << "Hello" << endl;
+}
