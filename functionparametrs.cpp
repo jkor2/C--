1806b21382e1e4206f1598677ad2939f6219e73d
Jake Korobellis <jkorobellis@gmail.com>
2023-07-23 22:49:43 -0400

    passing in function parameters

---
+++ functionparametrs.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu()
+{
+    cout << "1. Search" << endl;
+    cout << "2. View Record" << endl;
+    cout << "3. Quitting" << endl;
+    cout << endl;
+    cout << "Enter Selection" << endl;
+}
+
+int getInput()
+{
+
+    int input;
+    cin >> input;
+    return input;
+}
+
+void processSelection(int input)
+{
+    switch (input)
+    {
+    case 1:
+        cout << "Searching...." << endl;
+        break;
+    case 2:
+        cout << "Viewing..." << endl;
+        break;
+        ;
+    case 3:
+        cout << "Quitting..." << endl;
+
+    default:
+        cout << "Please select and item from the menu" << endl;
+    }
+}
+
+int main()
+{
+
+    showMenu();
+    int input = getInput();
+    processSelection(input);
+}
